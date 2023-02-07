4c5050731d4c54d526f970bcefa1b67f2b07f06b
Ajay <vkajay656@gmail.com>
2023-02-07 22:42:01 +0530

    uploading project

---
+++ src/com/techpalle/MyProgram.java
@@ -0,0 +1,28 @@
+package com.techpalle;
+
+import java.util.ArrayList;
+
+public class MyProgram {
+
+	public static void main(String[] args) {
+		//student.creating();
+       // student.inserting("ajay", "java", "abc@gmail.com");
+		//student.inserting("alby", "jee", "bac@gmail.com");
+		//student.updating("vkajay656@gmail.com", "sql", 2);
+		//student.delete(2);
+		/*ArrayList<student> resl=student.reading();
+		for(student item:resl)
+		{
+			System.out.println(item.sno);
+			System.out.println(item.sname);
+			System.out.println(item.sub);
+			System.out.println(item.email);
+			
+			System.out.println("********");
+			
+		}
+		*/
+		student.read();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Ajay
+ *
+ */
+module JDBCcrudproj {
+	requires java.sql;
+	requires java.desktop;
+}
